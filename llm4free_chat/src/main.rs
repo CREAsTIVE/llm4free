3e072b325acf2c1825595b6e823e8f0b60c3dcd8
CREAsTIVE <62616308+CREAsTIVE@users.noreply.github.com>
2025-06-28 17:17:19 +0500

    refactor: .idea + spacing
    Added .idea directory with project configuration files for IntelliJ IDEA. Also fixed minor whitespace formatting in llm4free_chat/src/main.rs.

--- llm4free_chat/src/main.rs
+++ llm4free_chat/src/main.rs
@@ -6,31 +6,31 @@ fn main() {
   println!("llm4free chat. Please, read README.MD and LEGAL_NOTICE.MD before using!\n");
 
   let completors: HashMap<String, Box<dyn BotChatCompletion>> = HashMap::new();
-  
-  
+
+
   match completors.get(&input!("Model name: ")) {
     Option::None => println!("Wrong model type!"),
     Option::Some(completor) => {
       let mut chat = Chat {
         messages: vec![]
       };
-      
+
       println!("Chat started!");
-      
+
       loop {
         let message = input!("> ");
         chat.messages.push(Message {
           author: Author::User,
           content: message
         });
-        
+
         let answer = completor.complete(&chat);
         println!("< {}", answer);
         chat.messages.push(Message {
           author: Author::Assistent,
           content: answer
         })
-      }   
+      }
     }
   }
 }
